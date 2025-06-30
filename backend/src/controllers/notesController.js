0b8587d2ce717399ac9aa6c8fb3edcca7d122916
freddycmh <fleddymok@gmail.com>
2025-06-30 11:52:41 -0400

    fixed the note controllers and also added a get method for specific ID

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,8 +2,22 @@ import Note from "../models/Note.js";
 
 export async function getAllNotes(req, res) {
     try {
-        const notes = await Note.find();
+        const notes = await Note.find().sort({createdAt:-1});//newest first
         res.status(200).json(notes);
+    } catch (error) {
+        console.error("error in getAllNotes", error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function getNoteById(req, res) {
+    try {
+        const note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).json({ message: "note not found" });
+        }
+        res.status(200).json(note);
+        
     } catch (error) {
         console.error("error in getAllNotes", error);
         res.status(500).json({ message: error.message });
@@ -27,15 +41,31 @@ export async function createNote (req, res){
 export async function updateNote(req, res) {
     try {
         const { title, content } = req.body
-        await Note.findByIdAndUpdate(req.params.id, { title, content });
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id, { title, content },{new:true});
 
-        res.status(200).json({message:"note updated successfully"});
+        if(!updatedNote){
+            return res.status(404).json({message:"note not found"});
+        }
+
+        res.status(200).json({updatedNote});
     } catch (error) {
         console.error("error in update note", error);
         res.status(500).json({ message: error.message });
     }
 }
 
-export function deleteNote(req, res) {
-    res.status(200).json({message:"note deleted successfully"});
+export async function deleteNote(req, res) {
+    try {
+        const deleteNote=await Note.findByIdAndDelete(req.params.id);
+        if(!deleteNote){
+            return res.status(404).json({message:"note not found"});
+        }
+        res.status(200).json({message:"note deleted"});
+        
+    } catch (error) {
+        console.error("error in delete note", error);
+        res.status(500).json({ message: error.message });
+        
+    }
+    
 }
--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController.js";
+import { createNote, deleteNote, getAllNotes, updateNote, getNoteById} from "../controllers/notesController.js";
 
 const router = express.Router()
 
 //get all
 router.get("/", getAllNotes);
+// get one
+router.get("/:id", getNoteById);
 //create
 router.post("/", createNote);
 //update
